87450bd8fe69c66d7bc5c0be95a306cc94d29c22
Artem Sidorenko <artoymsd@gmail.com>
2020-03-16 12:30:23 +0300

    fix default shouldSendEvent = true

--- Pod/Classes/BetterSegmentedControl.swift
+++ Pod/Classes/BetterSegmentedControl.swift
@@ -287,7 +287,7 @@ import UIKit
     /// - Parameters:
     ///   - index: The new index.
     ///   - animated: (Optional) Whether the change should be animated or not. Defaults to `true`.
-  public func setIndex(_ index: Int, animated: Bool = true, shouldSendEvent: Bool? = nil) {
+  public func setIndex(_ index: Int, animated: Bool = true, shouldSendEvent: Bool? = true) {
         guard normalSegments.indices.contains(index) else { return }
         
         let oldIndex = self.index
